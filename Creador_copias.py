b8e71cdde8a5f4c8a601fda6a1bb1adc94769785
AlfredoCruzado <64969169+AlfredoCruzado@users.noreply.github.com>
2022-07-08 16:49:30 -0500

    Creador de copia1

---
+++ Creador_copias.py
@@ -0,0 +1,6 @@
+import shutil
+
+
+Fuente = "C:\Users\con_acruzados\Documents\Reporte_Diario\prueba\resgistro_diario.xlsx"
+Destino = "C:\Users\con_acruzados\Documents\Reporte_Diario\prueba\resgistro_diariod.xlsx"
+shutil.copy(Fuente, Destino)
