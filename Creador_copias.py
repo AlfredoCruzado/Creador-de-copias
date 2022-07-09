bb0edc8eac75efd24ccb6fbc3f4babeb3bc1e14e
AlfredoCruzado <64969169+AlfredoCruzado@users.noreply.github.com>
2022-07-09 12:25:29 -0500

    instentos_de_rescribir_fecha

--- Creador_copias.py
+++ Creador_copias.py
@@ -1,8 +1,20 @@
 import shutil
 
+import pandas as pd
 
 Fuente = "C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diario.xlsx"
 Destino = "C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diariod.xlsx"
 
 shutil.copy(Fuente, Destino)
 #shutil.copy("resgistro_diario.xlsx","C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diariod.xlsx")
+df = pd.read_excel("resgistro_diariod.xlsx", sheet_name='Hoja1')
+df.describe()
+print("Columns")
+print(df.columns)
+
+column = df.columns[1]
+print(column)
+print("-" * len(column))
+
+for index, row in df.iterrows():
+    print(row[column])
