880338f71079d9890bbc33cec9c2a6cfa5c9913e
AlfredoCruzado <64969169+AlfredoCruzado@users.noreply.github.com>
2022-07-09 10:15:01 -0500

    copia el archivo

--- Creador_copias.py
+++ Creador_copias.py
@@ -1,6 +1,8 @@
 import shutil
 
 
-Fuente = "C:\Users\con_acruzados\Documents\Reporte_Diario\prueba\resgistro_diario.xlsx"
-Destino = "C:\Users\con_acruzados\Documents\Reporte_Diario\prueba\resgistro_diariod.xlsx"
+Fuente = "C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diario.xlsx"
+Destino = "C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diariod.xlsx"
+
 shutil.copy(Fuente, Destino)
+#shutil.copy("resgistro_diario.xlsx","C:/Users/con_acruzados/Documents/Reporte_Diario/prueba/resgistro_diariod.xlsx")
